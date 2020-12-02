676528f9393991b49ccf128536c0807c3a13deff
TANG CHENG LEE <virgil246@gmail.com>
2020-12-03 00:20:25 +0800

    try to fix catch error

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function EncodeSecretValue(key_id, key, value) {
 }
 const boostrap = async (octokit, owner, repo, name, value) => {
     try {
-        var res = await octokit.actions.getRepoPublicKey({ owner, repo }).catch(error => { Core.setFailed(error.message) })
+        var res = await octokit.actions.getRepoPublicKey({ owner, repo }).catch(error => { core.setFailed(error.message) })
         const key_id = res.data.key_id
         const key = res.data.key
 
@@ -30,7 +30,7 @@ const boostrap = async (octokit, owner, repo, name, value) => {
         const data = EncodeSecretValue(key_id, key, value)
         if (value.length > 0) {
 
-            res = await octokit.actions.createOrUpdateRepoSecret({ owner, repo, secret_name: name, data }).catch(error => { Core.setFailed(error.message) })
+            res = await octokit.actions.createOrUpdateRepoSecret({ owner, repo, secret_name: name, data }).catch(error => { core.setFailed(error.message) })
             console.log(`Here is response : ${res}`)
 
             if (res.status >= 400) {
@@ -41,7 +41,7 @@ const boostrap = async (octokit, owner, repo, name, value) => {
             }
         }
     } catch (error) {
-        Core.setFailed(error.message)
+        core.setFailed(error.message)
         console.error(error)
     }
 
@@ -55,7 +55,7 @@ try {
     const myToken = core.getInput('myToken');
     const octokit = github.getOctokit(token)
     octokit.actions.createOrUpdateRepoSecret()
-    boostrap(octokit, own_repo[0], own_repo[1], name, value)
+    boostrap(octokit, own_repo[0], own_repo[1], name, value).catch(error => core.setFailed(error.message))
 } catch (error) {
     core.setFailed(error.message);
 }
